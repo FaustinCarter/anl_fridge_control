47d0c4177b3923f3721f64e5b6ba1a19327ed634
spt3g <spt3g@axe.msd.anl.gov>
2017-03-16 11:47:36 -0500

    saves times

--- take_rt_mini.py
+++ take_rt_mini.py
@@ -4,6 +4,7 @@ import pydfmux
 import time
 import datetime
 import subprocess
+import pickle
 
 
 PID_high_temp = 0.650
@@ -13,8 +14,9 @@ sleeptime = 60
 logfile = '/home/spt3g/he10_logs/20170302_again_read.h5'
 
 ledgerman_path = '/home/spt3g/spt3g_software/dfmux/bin/ledgerman.py'
-R_data_path_0137 = '/home/spt3g/output/20170316/rt0137.nc'
-R_data_path_0135 = '/home/spt3g/output/20170315/rt0135.nc'
+data_dir = '/home/spt3g/output/20170316/'
+R_data_path_0137 = data_dir + 'rt0137.nc'
+R_data_path_0135 = data_dir + 'rt0135.nc'
 
 hwm_0137 = '/home/spt3g/hardware_maps/hwm_anl_20170306_W169_and_Rboards/hwm_0137.yml'
 hwm_0135 = '/home/spt3g/hardware_maps/hwm_anl_20170306_W169_and_Rboards/hwm_0135.yml'
@@ -23,11 +25,12 @@ sc.ChaseLS.set_heater_range(2)
 sc.ChaseLS.set_PID_temp(1, PID_high_temp)
 
 while float(gt.gettemp(logfile, 'UC Head'))<0.650:
-while float(gt.gettemp(logfile, 'UC Head'))<PID_low_temp:
+#while float(gt.gettemp(logfile, 'UC Head'))<PID_low_temp:
 	print "UC Head temp is " + str(gt.gettemp(logfile, 'UC Head'))
 	time.sleep(10)
 
-print "start time: " + str(time.time())
+starttime = time.time()
+print "start time: " + str(starttime)
 
 proc_ledgerman_0137 = subprocess.Popen(['python', ledgerman_path, hwm_0137, R_data_path_0137, '-s'])
 proc_ledgerman_0135 = subprocess.Popen(['python', ledgerman_path, hwm_0135, R_data_path_0135, '-s'])
@@ -54,5 +57,10 @@ while float(gt.gettemp(logfile, 'UC Head'))>0.400:
 proc_ledgerman_0137.terminate()
 proc_ledgerman_0135.terminate()
 
-print "end time: " + str(time.time())
+endtime=time.time()
+print "end time: " + str(endtime)
 
+startendtime = {'start_time':starttime, 'end_time':endtime}
+f=open(data_dir + 'rt_start_end_time.pkl')
+pickle.dump(startendtime, f)
+f.close()
