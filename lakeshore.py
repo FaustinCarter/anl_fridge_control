a769761273ae92b679e0f34c70f3599f77732a92
Faustin Carter <faustin.carter@gmail.com>
2017-03-20 20:58:13 -0500

    fix spacing issue in lakeshore

--- lakeshore.py
+++ lakeshore.py
@@ -5,22 +5,22 @@ class TempControl:
         if len(channames) != 4:
             print('ERROR: Incorrect number of channel names.')
             return
-        
+
         self.connex = serial.Serial(port='/dev/ttyr18', baudrate=9600, parity=serial.PARITY_ODD, stopbits=serial.STOPBITS_ONE, bytesize=serial.SEVENBITS, timeout=1)
         self.channel_names = channames
 
     def get_temps(self):
         self.connex.write('KRDG? %s\r\n'%(self.channel_names[0]))
-	w=self.connex.readline()
+        w=self.connex.readline()
         self.connex.write('KRDG? %s\r\n'%(self.channel_names[1]))
-	x=self.connex.readline()	
+        x=self.connex.readline()
         self.connex.write('KRDG? %s\r\n'%(self.channel_names[2]))
-	y=self.connex.readline()
+        y=self.connex.readline()
         self.connex.write('KRDG? %s\r\n'%(self.channel_names[3]))
-	z=self.connex.readline()
-	temps={str(self.channel_names[0]):w.strip('\r\n'), str(self.channel_names[1]):x.strip('\r\n'), str(self.channel_names[2]):y.strip('\r\n'), str(self.channel_names[3]):z.strip('\r\n')}
-	return temps
-    
+        z=self.connex.readline()
+        temps={str(self.channel_names[0]):w.strip('\r\n'), str(self.channel_names[1]):x.strip('\r\n'), str(self.channel_names[2]):y.strip('\r\n'), str(self.channel_names[3]):z.strip('\r\n')}
+        return temps
+
 #    def read_queue(self):
 #        x=self.connex.readline()
 #	return x
@@ -50,6 +50,6 @@ class TempControl:
 
     def config_output(self, output, mode, input):
         if output in [1,2] and mode in range(6) and input in range(5):
-		self.connex.write('OUTMODE %d,%d,%d,0\r\n'%(output, mode, input))
+        self.connex.write('OUTMODE %d,%d,%d,0\r\n'%(output, mode, input))
         else:
             print('ERROR: Heater output, mode, or input outside of allowed range!')
